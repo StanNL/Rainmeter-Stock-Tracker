b933da215489381748565165836344949fd6d703
Stan van Baarsen <stanvanbaarsen@hotmail.com>
2020-08-02 12:50:34 +0200

    critical bugfix

--- loadPrice.py
+++ loadPrice.py
@@ -9,28 +9,34 @@ def loadURL(url):
 
 # Feel free to change this to other APIs than alphavantage
 # The function should, however, return the following:
-# 		(price, success, datetime)
+# 		(price, success, datetime, openV, prevC)
 # where:
 #   price indicates the price that was retrieved
 #   success indicates whether the price was read from backup, or live from the remote API
 #   datetime (only when price was read from remote) indicates the date at which the stock price was live. As of right now, the time in this datetime object must be zero.
+#   openV (optional; the stock price when the exchange opened)
+#   prevC (optional; the stock price when the exchange last closed)
 def loadPrice(): 
-	response = loadURL('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=' + readConfig('api_key') + '&symbol=INGA.AMS&outputsize=compact&datatype=json')
+	response = loadURL('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=' + readConfig('api_key') + '&symbol=' + readConfig("stockName") + '&outputsize=compact&datatype=json')
 	d = json.loads(response)
 
 	if 'Global Quote' not in d:
 		writeLog("ERROR! API limit reached. Reading from backup...")
-		return (readBackup(), False, None)
+		return (readBackup(), False, None, 0)
 	else:
 		contents = d['Global Quote']
 		for k in contents.keys():
 			if len(k.split("price")) > 1:
 				price = contents[k]
+			if len(k.split("open")) > 1:
+				openV = contents[k]
+			if len(k.split("previous close")) > 1:
+				prevC = contents[k]
 			if len(k.split("latest")) > 1:
 				date = contents[k]
 
 		if k is not None:
 			writeLog("Successfully retrieved value " + price + 'EUR from remote.')
-			return (price, True, datetime.datetime.strptime(date, "%Y-%m-%d"))
+			return (price, True, datetime.datetime.strptime(date, "%Y-%m-%d"), openV, prevC)
 		else:
 			writeLog("FOUT! Hier gebeurde iets heel geks...")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
 import re
+import sys
 from sys import argv
+import os
 from fileManager import readConfig, readBackup, printDate, writeLog, parseAmount, setColour
 from loadPrice import loadPrice
 
@@ -9,61 +11,64 @@ red = "255, 0, 0"
 green = "0, 255, 0"
 white = "255, 255, 255"
 
-def retrieveRemote():
-	apicall = loadPrice()
-	price = apicall[0]
-	success = apicall[1]
-	stocktime = apicall[2]
-	print(parseAmount(price))
-	if success:
-		backupfile = open(rootFolder + "lastValue.txt", 'r')
-		oldBackup = backupfile.read()
-		backupfile.close()
-		if oldBackup.split('\n')[1] == str(datetime.datetime.combine(datetime.datetime.now().date(), datetime.datetime.min.time())):
-			#stock was last updated today
-			oldV = float(re.sub('[a-zA-Z]', '', oldBackup.split('\n')[0]))
-			newV = float(re.sub('[a-zA-Z]', '', price))
-			if oldV > newV:
-				setColour(red)
-				open(rootFolder + 'color.txt', 'w').write(red)
-			else:
-				if oldV < newV:
-					setColour(red)
-				else:
-					setColour(white)
-					open(rootFolder + 'color.txt', 'w').write(white)
-		else:
-			setColour(white)
 
-		backup = open(rootFolder + "lastValue.txt", 'w')
-		backup.write(price + '\n' + str(stocktime))
-	else:
-		backupv = readBackup()
-		writeLog("Retrieved value " + backupv + " from backup.")
-		print(backupv)
+def retrieveRemote():
+    apicall = loadPrice()
+    price = apicall[0]
+    success = apicall[1]
+    stocktime = apicall[2]
+    openV = apicall[3]
+    prevC = apicall[4]
+    print(parseAmount(price))
+    if success:
+        if(len(apicall) < 4):
+            setColour(white)
+        else:
+            if(float(prevC) > float(price) + 0.01):
+                setColour(red)
+            else:
+                if(float(prevC) < float(price) - 0.01):
+                    setColour(green)
+                else:
+                    setColour(white)
+        backup = open(rootFolder + "lastValue.txt", 'w')
+        backup.write(parseAmount(price) + '\n' + str(stocktime))
+    else:
+        setColour(white)
+        backupv = readBackup()
+        writeLog("Retrieved value " + backupv + " from backup.")
+        print(backupv)
+
 
 try:
-	if len(argv) > 1 and argv[1] == "rightClick":
-		writeLog("Force-refreshing because of right click!")
-		retrieveRemote()
-	else:
-		t = datetime.datetime.now()
-		oh = readConfig('openingHour') if readConfig('openingHour') is not None else 00
-		ch = readConfig('closingHour') if readConfig('closingHour') is not None else 23
-		om = readConfig('openingMinutes') if readConfig('openingMinutes') is not None else 00
-		cm = readConfig('closingMinutes') if readConfig('closingMinutes') is not None else 59
+    if len(argv) > 1 and argv[1] == "middleClick":
+        writeLog("Force-refreshing because of right click!")
+        retrieveRemote()
+    else:
+        t = datetime.datetime.now()
+        oh = readConfig('openingHour') if readConfig(
+            'openingHour') is not None else 00
+        ch = readConfig('closingHour') if readConfig(
+            'closingHour') is not None else 23
+        om = readConfig('openingMinutes') if readConfig(
+            'openingMinutes') is not None else 00
+        cm = readConfig('closingMinutes') if readConfig(
+            'closingMinutes') is not None else 59
 
-		beforeHours = (t.hour < oh or (t.hour is oh and t.minute < om))
-		afterHours = (t.hour > ch or (t.hour is ch and t.minute > cm))
-		weekend = (datetime.datetime.now().weekday() + 1) in (readConfig("closedDays") or [])
+        beforeHours = (t.hour < oh or (t.hour is oh and t.minute < om))
+        afterHours = (t.hour > ch or (t.hour is ch and t.minute > cm))
+        weekend = (datetime.datetime.now().weekday() +
+                   1) in (readConfig("closedDays") or [])
 
-		if beforeHours or afterHours or weekend:
-			print(readBackup())
-			setColour(white)
-			writeLog("Exchange closed! Read from backup.")
-		else:
-			retrieveRemote()
+        if beforeHours or afterHours or weekend:
+            print(readBackup())
+            setColour(white)
+            writeLog("Exchange closed! Read from backup.")
+        else:
+            retrieveRemote()
 
 except Exception as err:
-	writeLog("ERROR! Unknown error: " + str(err))
-
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    writeLog("ERROR! Unknown error: " + str(err) +
+             ". This error occured on line " + str(exc_tb.tb_lineno) + " in file " + fname)
